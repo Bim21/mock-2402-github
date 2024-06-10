44887c1d777279d3d4f94583936b900fe9795d3e
Bim21 <chiennguyen2198@gmail.com>
2024-06-10 10:48:17 +0700

    fix filter job v3

--- Job-Portal/src/main/java/com/vti/JobPortal/repositories/IJobRepository.java
+++ Job-Portal/src/main/java/com/vti/JobPortal/repositories/IJobRepository.java
@@ -10,9 +10,20 @@ import java.util.List;
 
 @Repository
 public interface IJobRepository extends MongoRepository<Job, Long> {
-    List<Job> findByJobFieldAndJobAddressAndLevelAndCareerJob(List<String> jobField, List<String> jobAddress,String level, List<String> careerJob);
-    List<Job> findByJobFieldIn(List<String> jobField);
+    List<Job> findByJobFieldAndJobAddressAndLevelAndCareerJobIn(String jobField, List<String> jobAddress,String level, List<String> careerJob);
+    List<Job> findByJobFieldIn(String jobField);
     List<Job> findByJobAddressIn(List<String> jobAddress);
     List<Job> findByLevel(String level);
     List<Job> findByCareerJobIn(List<String> careerJob);
+    List<Job> findByJobFieldAndJobAddressIn(String jobField, List<String> jobAddress);
+    List<Job> findByJobFieldAndLevel(String jobField, String level);
+    List<Job> findByJobFieldAndCareerJobIn(String jobField, List<String> careerJob);
+    List<Job> findByJobFieldAndJobAddressAndLevelIn(String jobField, List<String> jobAddress, String level);
+    List<Job> findByJobFieldAndLevelAndCareerJobIn(String jobField, String level, List<String> careerJob);
+    List<Job> findByJobFieldAndJobAddressAndCareerJobIn(String jobField, List<String> jobAddress, List<String> careerJob);
+    List<Job> findByJobAddressAndLevelIn(List<String> jobAddress, String level);
+    List<Job> findByJobAddressAndCareerJobIn(List<String> jobAddress, List<String> careerJob);
+    List<Job> findByLevelAndCareerJobIn(String level, List<String> careerJob);
+
+    List<Job> findByJobAddressAndLevelAndCareerJobIn(List<String> jobAddress, String level, List<String> careerJob);
 }

5e2751406525e941c978ddc76ed0fc2205fa589c
PhanDuc96 <2cauranh@gmail.com>
2024-07-01 21:46:54 +0700

    congturated

--- recruitment-app/client/src/pages/EmployerPage/section/UserTable.js
+++ recruitment-app/client/src/pages/EmployerPage/section/UserTable.js
@@ -2,12 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Button, Space, Table, Tag } from "antd";
 import applicantService from "../../../services/applicantService";
 import { getUserInfo } from "../../../utils/funcHelpers";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CloseCircleOutlined, SendOutlined } from "@ant-design/icons";
+import ConfirmDeleteModal from "./ConfirmDeleteModal ";
 
 const UserTable = () => {
   const [userApplyData, setUserApplyData] = useState([]);
+  const [isModalDeleteOpen, setIsModalDeleteOpen] = useState(false);
+  const [itemId, setItemId] = useState(null);
+
   const userInfo = getUserInfo();
+  const navigate = useNavigate();
+
+  const handleOpenDeleteModal = (id) => {
+    setItemId(id);
+    setIsModalDeleteOpen(true);
+  };
+
+  const handleCloseDeleteModal = () => {
+    setIsModalDeleteOpen(false);
+  };
 
   const getUserApply = async () => {
     try {
@@ -22,6 +36,26 @@ const UserTable = () => {
   useEffect(() => {
     getUserApply();
   }, []);
+
+  const handleAccept = () => {
+    try {
+      navigate("/notification");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlReject = async () => {
+    // try {
+    //   await applicantService.delete(itemId);
+    //   getUserApply();
+    //   toast.success("Đã từ chối");
+    //   setIsModalDeleteOpen(false);
+    // } catch (error) {
+    //   console.error("đã có lỗi", error);
+    // }
+    // }
+  };
 
   const columns = [
     {
@@ -54,12 +88,25 @@ const UserTable = () => {
       dataIndex: "actions",
       render: (value, record, index) => (
         <Space size="small">
-          <Button type="primary" icon={<SendOutlined />}>
-            Gửi mail
+          <Button type="primary" icon={<SendOutlined />} onClick={handleAccept}>
+            Chấp nhận
           </Button>
-          <Button type="primary" danger icon={<CloseCircleOutlined />}>
+          <Button
+            type="primary"
+            danger
+            icon={<CloseCircleOutlined />}
+            onClick={() => handleOpenDeleteModal()}
+          >
             Từ chối
           </Button>
+          <ConfirmDeleteModal
+            isOpen={isModalDeleteOpen}
+            onClose={handleCloseDeleteModal}
+            onConfirm={handlReject}
+            title={"Xác nhận từ chối"}
+            deps={"Bạn có chắc chắn từ chối ứng viên này?"}
+            button={"Từ chối"}
+          />
         </Space>
       ),
     },
